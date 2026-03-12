7e8640f48e37d52c3f034e109f720c2292120e69
Ammon Knaupp <adknaupp@gmail.com>
2026-03-12 14:36:25 -0400

    Format file metadata; add path breadcrumbs, globus app link

--- dry_lab_notebook/views.py
+++ dry_lab_notebook/views.py
@@ -1,10 +1,12 @@
+from pathlib import Path
 import typing as t
 import os
+from datetime import datetime
 import django
 from django.conf import settings
 from django.views.generic import View
 from django.shortcuts import render
-from urllib.parse import quote_plus, unquote_plus
+from urllib.parse import quote_plus, unquote, unquote_plus, urlencode
 
 from globus_portal_framework.gsearch import (
     get_template,
@@ -27,6 +29,21 @@ def get_client_from_client_credentials() -> globus_sdk.SearchClient:
     )
     client = globus_sdk.SearchClient(authorizer=cc_authorizer)
     return client
+
+def generate_globus_url(collection_id, path="/~/"):
+    base_url = "https://app.globus.org/file-manager"
+    
+    # Define parameters for the URL
+    params = {
+        "origin_id": collection_id,
+        "origin_path": path
+    }
+    
+    # URL encode the parameters to handle spaces and special characters
+    query_string = urlencode(params)
+    
+    return f"{base_url}?{query_string}"
+
 
 class SearchView(DGPFSearchView):
 
@@ -72,7 +89,15 @@ class DetailView(View):
     def get_context_data(self, index: str, subject: str) -> t.Mapping[str, str]:
         """Call globus_portal_framework.gsearch.get_subject using the index, subject, and user
         and return the result."""
-        return get_subject(index, subject)
+        data = get_subject(index, subject)
+        entry = data['all'][0] # data from the index that will be rendered as a table
+        data['title'] = entry['name'] # special key used by template `detail-nav.html`
+        path = Path(unquote(entry["path"]))
+        data['globus_app_link'] = generate_globus_url(entry['collection'], str(path.parent))
+        entry["last_modified"] = datetime.fromisoformat(entry["last_modified"])
+        entry["date_indexed"] = datetime.fromisoformat(entry["date_indexed"])
+        data["dirs"] = {name: "/" + "/".join(parts[:i+1]) for parts in [[pt for pt in str(path.parent).split('/') if pt]] for i, name in enumerate(parts)}
+        return data
 
     def get(self, request: django.http.HttpRequest, index: str, subject: str):
         """Get context data, and return a rendered search view, selecting the template with
